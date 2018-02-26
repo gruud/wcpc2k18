f5eaff8bbba75793503ece502733d2a1b5fd18d5
gruud <6158418+gruud@users.noreply.github.com>
2018-02-26 11:39:39 +0100

    Initialisation du projet Symfony et du bundle

---
+++ src/WCPC2K18Bundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace WCPC2K18Bundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('WCPC2K18Bundle:Default:index.html.twig');
+    }
+}
---
+++ src/WCPC2K18Bundle/WCPC2K18Bundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace WCPC2K18Bundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class WCPC2K18Bundle extends Bundle
+{
+}

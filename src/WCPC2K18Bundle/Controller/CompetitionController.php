81bf1b9c591e759a974c1effa3fb75d54c3cb3ae
Jean-Yves Dumollard <jean-yves.dumollard@dgfip.finances.gouv.fr>
2018-05-18 15:49:50 +0200

    test base de données

---
+++ src/WCPC2K18Bundle/Controller/CompetitionController.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace WCPC2K18Bundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use WCPC2K18Bundle\Entity\Competition;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Description of CompetitionController
+ *
+ * @author jean-yves
+ */
+class CompetitionController extends Controller {
+    
+    
+        public function affichageAction() {
+
+        $manager = $this->getDoctrine()->getManager();
+
+        $competitions = $manager->getRepository("WCPC2K18Bundle:Competition")->findAll();
+
+        return $this->render("WCPC2K18Bundle:competition:competition.html.twig", array('competitions' => $competitions));
+    }
+}
---
+++ src/WCPC2K18Bundle/Controller/EquipeController.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace WCPC2K18Bundle\Controller;
+
+/**
+ * Description of EquipeController
+ *
+ * @author jean-yves
+ */
+class EquipeController {
+    //put your code here
+}

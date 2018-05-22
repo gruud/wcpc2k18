43219f0b553cb647f0cf2bcf5d2fcb0f13c4559d
Jean-Yves Dumollard <jean-yves.dumollard@dgfip.finances.gouv.fr>
2018-05-22 16:15:34 +0200

    insertion des données et formulaire de saisie

---
+++ src/WCPC2K18Bundle/Controller/RencontreController.php
@@ -0,0 +1,79 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace WCPC2K18Bundle\Controller;
+
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use WCPC2K18Bundle\Entity\Rencontre;
+use WCPC2K18Bundle\Entity\Equipe;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Description of RencontreController
+ *
+ * @author jean-yves
+ */
+class RencontreController extends Controller {
+    
+ public function addAction(Request $request) {
+        $rencontre = new Rencontre();
+        $formBuilder = $this->get('form.factory')->createBuilder('form', $rencontre);
+
+        $formBuilder
+                ->add('date', 'datetime')
+                ->add('lieu', 'text')
+                ->add('equipeDom',
+                      'entity',
+                       array ('class'=> 'WCPC2K18Bundle\Entity\Equipe',
+                      'property' => 'nom',
+                       'label' => 'Domicile',
+                       'multiple' => false,
+                       'required' => true))
+                ->add('equipeExt',
+                      'entity',
+                       array ('class'=> 'WCPC2K18Bundle\Entity\Equipe',
+                      'property' => 'nom',
+                       'label' => 'Visiteur',
+                       'multiple' => false,
+                       'required' => true))
+                ->add('journee', 'text')
+                         
+                ->add('save', 'submit');
+        $form = $formBuilder->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+
+
+
+            $rencontreCour = $this->getDoctrine()->getManager();
+
+            $rencontreCour->persist($rencontre);
+
+            $rencontreCour->flush();
+
+            return $this->redirectToRoute("rencontre");
+        }
+        return $this->render("WCPC2K18Bundle:rencontre:addRencontre.html.twig", array('form' => $form->createView()));
+    }   
+    
+    
+    public function affichageAction() {
+
+        $manager = $this->getDoctrine()->getManager();
+
+        $rencontres = $manager->getRepository("WCPC2K18Bundle:Rencontre")->findAll();
+
+        return $this->render("WCPC2K18Bundle:rencontre:rencontre.html.twig", array('rencontres' => $rencontres));
+    }
+    
+    
+}

95c69fd5bffccda783cfc2c43bc54e5fe6874743
Jean-Yves Dumollard <jean-yves.dumollard@dgfip.finances.gouv.fr>
2018-05-28 12:32:11 +0200

    add src/WCPC2K18Bundle/Form/

---
+++ src/WCPC2K18Bundle/Form/PredictionType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace WCPC2K18Bundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PredictionType extends AbstractType {
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        $builder->add('double')
+                ->add('nbButTrDom')
+                ->add('nbButProlDom')
+                ->add('nbTabDom')
+                ->add('nbButTrExt')
+                ->add('nbButProlExt')
+                ->add('nbTabExt')
+                ->add('prolongation')
+                ->add('tirAuBut')
+                ->add('save', 'submit');
+
+    }
+
+/**
+     * {@inheritdoc}
+     */
+
+    public function configureOptions(OptionsResolver $resolver) {
+        $resolver->setDefaults(array(
+            'data_class' => 'WCPC2K18Bundle\Entity\Prediction'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix() {
+        return 'wcpc2k18bundle_prediction';
+    }
+
+}
